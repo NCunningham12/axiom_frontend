d0a0b85148af3fd08e62b834dc84831d87084c57
Nathan Cunningham <counterweight22@gmail.com>
2025-10-14 00:12:56 -0700

    Added all concepts for 8th grade

--- src/curriculum/eighth.js
+++ src/curriculum/eighth.js
@@ -13,13 +13,13 @@ export default {
               id: 'rat-vs-irrat',
               slug: 'determining-irrationals',
               name: 'Rational vs. Irrational Numbers',
-              standard: '',
+              standard: '8.NS.A.1',
             },
             {
               id: 'rat-approx',
               slug: 'approximating-rationals',
               name: 'Rational Approximation',
-              standard: '',
+              standard: '8.NS.A.2',
             },
           ],
           id: 'square-roots',
@@ -29,11 +29,13 @@ export default {
               id: 'square-roots',
               slug: 'solving-perfect-squares',
               name: 'Square Roots',
+              standard: '8.NS.A.2',
             },
             {
               id: 'sqrt-approx-nl',
               slug: 'approximating-square-roots',
               name: 'Square Root Approximation on a Number Line',
+              standard: '8.NS.A.2',
             },
           ],
         },
@@ -78,6 +80,180 @@ export default {
               standard: '8.EE.A.1',
             },
             // { id: '', name: '', standard: '' },
+          ],
+        },
+        {
+          id: 'scientific-notation',
+          name: 'Scientific Notation',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'slope-intercept-form',
+          name: 'Slope Intercept Form',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'linear-equations',
+          name: 'Linear Equations',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'systems-of-linear-equations',
+          name: 'Systems of Linear Equations',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 'domain3',
+      name: 'Functions',
+      concepts: [
+        {
+          id: 'function-representations',
+          name: 'Function Representations',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'qualitative-graphs',
+          name: 'Qualitative Graphs',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 'domain4',
+      name: 'Geometry',
+      concepts: [
+        {
+          id: 'transformations',
+          name: 'Transformations',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'triangles',
+          name: 'Triangles',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'parallels-with-transversals',
+          name: 'Parallels With Transversals',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'pythagorean-theorem',
+          name: 'Pythagorean Theorem',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'volume-curved-figures',
+          name: 'Volume Of Curved Figures',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 'domain5',
+      name: 'Statistics & Probability',
+      concepts: [
+        {
+          id: 'scatter-plots',
+          name: 'Scatter Plots',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
+          ],
+        },
+        {
+          id: 'relative-frequency',
+          name: 'Relative Frequency',
+          skills: [
+            {
+              id: '',
+              slug: '',
+              name: '',
+              standard: '',
+            },
           ],
         },
       ],
