35e0fbeeb454232c2ac79802c8d880bf5703230f
Nathan Cunningham <counterweight22@gmail.com>
2026-07-29 15:11:27 -0700

    Scaffolded basic static classes page CSS

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import "./Classes.css"
+import React from 'react';
+import './Classes.css';
 
 const Classes = () => {
   return (
-    <div>Classes</div>
-  )
-}
+    <div className="classes-container">
+      <h1 className="classes-title">Classes</h1>
+      <div className="links">
+        - Link 1 <br />
+        - Link 2 <br />
+        - Link 3 <br />
+      </div>
+      <div className="cards">
+        <div className="class-card">Card 1</div>
+        <div className="class-card">Card 2</div>
+        <div className="class-card">Card 3</div>
+        <div className="class-card">Card 4</div>
+        <div className="class-card">Card 5</div>
+        <div className="class-card">Card 6</div>
+      </div>
+    </div>
+  );
+};
 
-export default Classes
+export default Classes;
